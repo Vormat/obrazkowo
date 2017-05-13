84f863724521581a133b0f106f07ebc7026b5a80
vormat <biuro@vormat.pl>
2017-05-13 13:38:11 +0200

    13.05.2017_13:36

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,5 +83,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'obrazkowo.herokuapp.com'}
+  config.action_mailer.default_url_options = { host: 'kiermowicz.herokuapp.com'}
 end
